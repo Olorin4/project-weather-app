944bcadee55cf08f79f227dafe2d62f8bfa0a962
Nick Kalas <nick.kalas@proton.me>
2024-09-09 16:06:15 +0300

    Fix weather grid

--- src/display-weather.js
+++ src/display-weather.js
@@ -1,4 +1,5 @@
-// displayWeather.js renders weather data to the DOM.
+//  displayWeather.js creates new DOM elements and
+//  renders weather data in the main content section.
 
 import { fetchWeather } from "./get-data";
 import { processWeatherData } from "./process-data";
@@ -19,8 +20,30 @@ function renderTitle(location) {
     title.textContent = location;
 }
 
-export async function displayWeather(userLocation) {
+function createHeaders() {
+    const weatherDisplay = document.querySelector(".weather-display");
+    const currentWeather = document.querySelector(".current-weather-container");
+    const weaklyWeather = document.querySelector(".weakly-weather-container");
+    const header1 = document.createElement("h3");
+    const header2 = document.createElement("h3");
+    header1.textContent = "Today's Weather";
+    header2.textContent = "Weakly Weather";
+    weatherDisplay.insertBefore(header1, currentWeather);
+    weatherDisplay.insertBefore(header2, weaklyWeather);
+}
+
+function createWeatherCard() {
+    
+}
+
+function renderCurrentWeather() {
+    createHeaders();
+    createWeatherCard();
+}
+
+async function displayWeather(userLocation) {
     const rawWeatherData = await fetchWeather(userLocation);
     const processedWeatherData = processWeatherData(rawWeatherData);
     renderTitle(processedWeatherData.currentWeather.location);
+    renderCurrentWeather();
 }
