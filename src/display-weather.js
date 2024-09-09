e71b42ecfc2d1a4ccaebf2b2444e55550e8aea26
Nick Kalas <nick.kalas@proton.me>
2024-09-09 20:20:18 +0300

    Create function for creating DOM elements related to weather

--- src/display-weather.js
+++ src/display-weather.js
@@ -4,6 +4,47 @@
 import { fetchWeather } from "./get-data";
 import { processWeatherData } from "./process-data";
 
+function renderTitle(location) {
+    const title = document.querySelector("h2");
+    title.textContent = location;
+}
+
+function createHeader(text, container) {
+    const weatherDisplay = document.querySelector(".weather-display");
+    const header = document.createElement("h3");
+    header.textContent = `${text} Weather`;
+    weatherDisplay.insertBefore(header, container);
+}
+
+function createWeatherCard(day, container) {
+    const weatherCard = document.createElement("div");
+    weatherCard.classList.add("weather-card");
+    weatherCard.dataset.day = `${day}`;
+    container.appendChild(weatherCard);
+}
+
+function renderCurrentWeather() {
+    const currentWeather = document.querySelector(".current-weather-container");
+    createHeader("Today's", currentWeather);
+    createWeatherCard(1, currentWeather);
+}
+
+function renderWeaklyWeather() {
+    const weaklyWeather = document.querySelector(".weakly-weather-container");
+    createHeader("Weakly", weaklyWeather);
+    for (let day = 2; day <= 7; day++) {
+        createWeatherCard(day, weaklyWeather);
+    }
+}
+
+async function displayWeather(userLocation) {
+    const rawWeatherData = await fetchWeather(userLocation);
+    const processedWeatherData = processWeatherData(rawWeatherData);
+    renderTitle(processedWeatherData.currentWeather.location);
+    renderCurrentWeather();
+    renderWeaklyWeather();
+}
+
 export function getLocation() {
     const searchInput = document.querySelector(".search-bar");
     searchInput.addEventListener("change", () => {
@@ -13,37 +54,4 @@ export function getLocation() {
         console.log(`User location: ${userLocation}`);
         displayWeather(userLocation);
     });
-}
-
-function renderTitle(location) {
-    const title = document.querySelector("h2");
-    title.textContent = location;
-}
-
-function createHeaders() {
-    const weatherDisplay = document.querySelector(".weather-display");
-    const currentWeather = document.querySelector(".current-weather-container");
-    const weaklyWeather = document.querySelector(".weakly-weather-container");
-    const header1 = document.createElement("h3");
-    const header2 = document.createElement("h3");
-    header1.textContent = "Today's Weather";
-    header2.textContent = "Weakly Weather";
-    weatherDisplay.insertBefore(header1, currentWeather);
-    weatherDisplay.insertBefore(header2, weaklyWeather);
-}
-
-function createWeatherCard() {
-    
-}
-
-function renderCurrentWeather() {
-    createHeaders();
-    createWeatherCard();
-}
-
-async function displayWeather(userLocation) {
-    const rawWeatherData = await fetchWeather(userLocation);
-    const processedWeatherData = processWeatherData(rawWeatherData);
-    renderTitle(processedWeatherData.currentWeather.location);
-    renderCurrentWeather();
 }
